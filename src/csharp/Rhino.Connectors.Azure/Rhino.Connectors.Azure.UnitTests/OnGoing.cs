91fb2ed0d466900c1188fe78e2f145e5bd794ecc
gravity-api <gravity.api@outlook.com>
2021-10-21 23:18:01 +0300

    update bug manager issue

--- src/csharp/Rhino.Connectors.Azure/Rhino.Connectors.Azure.UnitTests/OnGoing.cs
+++ src/csharp/Rhino.Connectors.Azure/Rhino.Connectors.Azure.UnitTests/OnGoing.cs
@@ -19,14 +19,7 @@ namespace Rhino.Connectors.Azure.UnitTests
         [TestMethod]
         public void Test()
         {
-            var json = File.ReadAllText(@"D:\garbage\azure.txt");
-            var c = JsonSerializer.Deserialize<RhinoConfiguration>(json, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
-            var connector = new AzureConnector(c);
-            connector.ProviderManager.DeleteTestRun("all");
-            connector.Invoke();
+
         }
     }
 }
